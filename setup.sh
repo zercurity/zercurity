c80ab8aefce24e9bee5a3cbc5553700374cd6bac
Zercurity <admin@zercurity.com>
2021-03-14 12:23:27 +0000

    Improved setup and update scripts to warn about common pitfalls

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
-
 #!/bin/bash
 set -e
+
+if ! ( command -v sudo > /dev/null ); then
+  echo "sudo is required in order to install and configure Zercurity
+Please run the commands below to install sudo:
+"
+  echo "su -"
+  echo "apt -y install sudo"
+  echo "echo '$(whoami) ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/$(whoami) ;"
+  echo "chmod 440 /etc/sudoers.d/$(whoami) ;"
+  echo "exit"
+  exit 1
+fi
 
 INSTALL_PATH="/var/lib/zercurity"
 ENV_FILE="$INSTALL_PATH/production.env"
@@ -54,3 +65,18 @@ fi
 sudo -- sh -c "echo \"127.0.0.1 $ZERCURITY_DOMAIN app.$ZERCURITY_DOMAIN api.$ZERCURITY_DOMAIN download.$ZERCURITY_DOMAIN\" >> /etc/hosts"
 
 ./update.sh
+
+echo "Zercurity has now been successfully setup."
+echo ""
+echo "Please visit each of the following URLs to both ensure
+a successful deployment but also accept the self-signed
+certificates. Otherwise, the app will not load:"
+echo ""
+echo "-> https://api.$ZERCURITY_DOMAIN/"
+echo "-> https://download.$ZERCURITY_DOMAIN/"
+echo "-> https://app.$ZERCURITY_DOMAIN/"
+echo ""
+echo "You can create your first user with the following command:"
+echo ""
+echo "docker exec zercurity_backend_1 ./zercurity --register --name \"$(whoami)\" --email \"your@email.com\""
+echo ""
--- update.sh
+++ update.sh
@@ -6,21 +6,40 @@ ENV_FILE="$INSTALL_PATH/production.env"
 
 if [[ ! -f "$ENV_FILE" ]]; then
   echo "Please run ./setup.sh first."
+  echo "https://github.com/zercurity/"
   exit 1
 fi
 
-if ! ( systemctl --all --type service | grep -q "docker" ); then
+if ! ( command -v docker > /dev/null ); then
   echo "Docker is not installed."
+  echo "https://docs.docker.com/engine/install/debian/"
   exit 1
 fi
 
 if ! ( command -v docker-compose > /dev/null ); then
   echo "Docker Compose is not installed"
+  echo "sudo pip3 install docker-compose"
   exit 1
+fi
+
+if ! ( command -v pip3 > /dev/null && pip3 list --format columns | grep docker-compose > /dev/null ); then
+  echo "!! WARNING !!"
+  echo "Docker compose is installed. However, not using pip3."
+  echo "Please ensure you are using the latest version of docker-compose
+asearlier versions do not support the --env-file parameter."
+  echo "If you receive help information from the docker-compose command
+  then it is most likely out-of-date."
+  echo ""
+  echo "sudo apt remove docker-compose"
+  echo "sudo pip3 install docker-compose"
+  echo "!! WARNING !!"
+  sleep 1
 fi
 
 if ! ( command -v git > /dev/null ); then
   echo "Git is not installed"
+  echo "Ubuntu/Debian: sudo apt install git"
+  echo "RHEL: sudo yum install git"
   exit 1
 fi
 
@@ -46,17 +65,17 @@ git pull
 sudo cp ./docker-compose.yml "$INSTALL_PATH/docker-compose.yml"
 
 # shellcheck disable=SC2154
-if ! ( getent group docker | grep -q "\b${whoami}\b" ) && (( EUID != 0 )); then
+if ! ( getent group docker | grep -q "\b$(whoami)\b" ) && (( EUID != 0 )); then
   echo "User not in the docker group. Or not root.
-  Use the following command to add your user to the docker group:
+Use the following command to add your user to the docker group:
   "
-  echo "sudo usermod -aG docker ${whoami}"
+  echo "sudo usermod -aG docker $(whoami)"
   exit 1
 fi
 
 if ! ( systemctl is-active --quiet docker ); then
   echo "Docker service not running. Please start the docker service
-  "
+"
   echo "sudo systemctl start docker"
   exit 1
 fi
@@ -70,10 +89,10 @@ if ( systemctl is-enabled --quiet zercurity ); then
   sudo systemctl restart zercurity
 else
   echo "Zercurity service not available. Please restart manually.
-  "
+"
   echo "sudo systemctl restart zercurity"
   echo "
-  or
+or
   "
   echo "sudo docker-compose --env-file $ENV_FILE up"
 fi
@@ -81,3 +100,10 @@ fi
 echo "Removing old containers .."
 
 docker image prune -f
+
+echo "Checking deployment status .."
+
+sleep 10
+
+curl -k -s "https://api.$ZERCURITY_DOMAIN/v1/healthcheck" | \
+    python3 -c "import sys, json; print(json.dumps(json.load(sys.stdin)['version'], indent=2))"
